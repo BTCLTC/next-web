0995b29bc76a4b2eb2224b3171d46f24cab495bf
liuxin <admin@github.xyz>
2022-06-01 15:38:18 +0800

    clear

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,26 +6,11 @@ import '../styles/pages/Home.scss'
 import '../styles/layouts/Header.scss'
 import '../styles/layouts/Footer.scss'
 
-import Script from 'next/script'
-
 import Layout from '../layouts/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-      />
-      <Script id="ga-analytics">
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-            `}
-      </Script>
       <Component {...pageProps} />
     </Layout>
   )
